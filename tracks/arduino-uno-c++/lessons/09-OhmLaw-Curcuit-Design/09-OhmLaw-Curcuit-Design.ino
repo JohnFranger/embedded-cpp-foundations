90dddb9bea604fccadbe1f042736f132506452b2
John <johndouglasfranger@gmail.com>
2026-08-23 20:41:03 -0600

    Finished lesson 9 with notes, no homework

---
+++ tracks/arduino-uno-c++/lessons/09-OhmLaw-Curcuit-Design/09-OhmLaw-Curcuit-Design.ino
@@ -0,0 +1,19 @@
+/**
+ * Lesson 09 — Ohm's Law and Simple Circuit Design
+ 
+No code this lesson, just understanding Ohm's Law, the relationships, and how to measure resistance in a circuit. 
+
+Will attach a pdf of my notes. 
+
+No Homework
+ */
+
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
---
+++ tracks/arduino-uno-c++/lessons/09-OhmLaw-Curcuit-Design/README.md
@@ -0,0 +1,11 @@
+# Lesson 08 — PWM LED Dimmer
+
+
+## Mathematical Proof / Analysis
+
+_Justify the component values and expected behavior._
+
+- **(Ohm's law):**
+  V = IR or I = V/R
+  Current is proportional to voltage and inversely proportional to resistance.
+
